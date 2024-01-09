59dbd3fce8ba86939a1b1464a63df342b419c006
Adolpho Piazza <adolphopiazza@gmail.com>
2024-01-09 19:13:25 -0300

    feat: set worlds view opacity to zero when view is loading to only show the progress view, and implemented refreshable

--- Tibia More/Features/Worlds/WorldsListView.swift
+++ Tibia More/Features/Worlds/WorldsListView.swift
@@ -15,13 +15,13 @@ struct WorldsListView: View {
         NavigationStack(path: $viewModel.navigationPath) {
             Form {
                 Section("About") {
-                    LabeledContent("Players Online", value: String(viewModel.model.playersOnline))
-                    LabeledContent("Record Players", value: String(viewModel.model.recordPlayers))
-                    LabeledContent("Record Date", value: viewModel.model.recordDate.formatDate(with: .yyyyMMddTHHmmssZ))
+                    LabeledContent("Players Online", value: String(viewModel.model?.playersOnline ?? 0))
+                    LabeledContent("Record Players", value: String(viewModel.model?.recordPlayers ?? 0))
+                    LabeledContent("Record Date", value: viewModel.model?.recordDate.formatDate(with: .yyyyMMddTHHmmssZ) ?? "")
                 }
                 
                 Section("Worlds") {
-                    List(viewModel.model.regularWorlds, id: \.name) { world in
+                    List(viewModel.model?.regularWorlds ?? [], id: \.name) { world in
                         WorldsListViewRow(model: world)
                             .contentShape(Rectangle())
                             .onTapGesture {
@@ -32,12 +32,17 @@ struct WorldsListView: View {
             }
             .fontDesign(.serif)
             .navigationTitle(viewModel.viewTitle)
+            .refreshable {
+                viewModel.model = nil
+                await viewModel.fetch()
+            }
             .navigationDestination(for: NavigationRoutes.Worlds.self, destination: { route in
                 switch route {
                 case .details(let world):
                     WorldsDetailsView(viewModel: WorldsDetailsViewModel(world: world))
                 }
             })
+            .opacity(viewModel.isLoading ? 0 : 1)
             .overlay {
                 if viewModel.isLoading {
                     ProgressView()
