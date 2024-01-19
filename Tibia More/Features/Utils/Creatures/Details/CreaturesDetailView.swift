1986e384ee5f6f5fb2cd096251acc4550318410b
Adolpho Piazza <adolphopiazza@gmail.com>
2024-01-19 20:19:50 -0300

    feat: completed creatures detail screen

--- Tibia More/Features/Utils/Creatures/Details/CreaturesDetailView.swift
+++ Tibia More/Features/Utils/Creatures/Details/CreaturesDetailView.swift
@@ -28,11 +28,44 @@ struct CreaturesDetailView: View {
                 }
 
                 if let creatureDescription = viewModel.model?.description {
-                    DisclosureGroup("Creature description") {
+                    DisclosureGroup("Creature Description") {
                         Text(creatureDescription)
                     }
                 }
             }
+            
+            Section("About") {
+                // Let's use worlds details view row but to-do: create a more generic
+                // view to use across the app
+                WorldsDetailsViewRow("Behaviour", value: viewModel.model?.behaviour, orientation: .vertical)
+                WorldsDetailsViewRow("Hitpoints", value: viewModel.model?.hitpoints)
+                WorldsDetailsViewRow("Loot List", value: viewModel.model?.lootList?.joined(separator: ", "), orientation: viewModel.model?.lootList?.count == 1 ? .horizontal : .vertical)
+                WorldsDetailsViewRow("Experience Points", value: viewModel.model?.experiencePoints)
+                WorldsDetailsViewRow("Immune to", value: viewModel.model?.immune?.joined(separator: ", "))
+                WorldsDetailsViewRow("Strong against", value: viewModel.model?.strong?.joined(separator: ", "))
+                WorldsDetailsViewRow("Weakness for", value: viewModel.model?.weakness?.joined(separator: ", "))
+                
+                if let seeInvisible = viewModel.model?.seeInvisible {
+                    WorldsDetailsViewRow("See invisible", value: seeInvisible ? "✅" : "🚫")
+                }
+                
+                if let beParalysed = viewModel.model?.beParalysed {
+                    WorldsDetailsViewRow("Can be Paralysed", value: beParalysed ? "✅" : "🚫")
+                }
+                
+                if let beSummoned = viewModel.model?.beSummoned, beSummoned {
+                    DisclosureGroup("Summon with") {
+                        Text("\(String(viewModel.model?.summonedMana ?? 0)) points of mana")
+                    }
+                }
+                
+                if let beConvinced = viewModel.model?.beConvinced, beConvinced {
+                    DisclosureGroup("Convince with") {
+                        Text("\(String(viewModel.model?.convincedMana ?? 0)) points of mana")
+                    }
+                }
+            }
+            .opacity(viewModel.hasError || viewModel.isLoading ? 0 : 1)
         }
         .fontDesign(.serif)
         .navigationTitle(viewModel.model?.name ?? "")
@@ -55,5 +88,5 @@ struct CreaturesDetailView: View {
 }
 
 #Preview {
-    CreaturesDetailView(viewModel: CreaturesDetailViewModel(creatureRace: "acidblob"))
+    CreaturesDetailView(viewModel: CreaturesDetailViewModel(creatureRace: "rotworm"))
 }
