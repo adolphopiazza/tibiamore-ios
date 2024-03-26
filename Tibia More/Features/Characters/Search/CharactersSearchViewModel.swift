10f2ac223c24e91aef01d3e500815b62a2a2fc3f
Adolpho Piazza <adolphopiazza@gmail.com>
2024-03-26 16:03:35 -0300

    bugfix: fixed a bug where the loading stopped before the navigation push, leading to half a second of enabled button and if tapped messed up the navigation

--- Tibia More/Features/Characters/Search/CharactersSearchViewModel.swift
+++ Tibia More/Features/Characters/Search/CharactersSearchViewModel.swift
@@ -17,27 +17,41 @@ final class CharactersSearchViewModel {
     var hasError: Bool = false
     var model: CharacterModel?
     
-    @MainActor func fetch() async {
+    @MainActor func fetchAllInfo() async {
         defer {
             self.isLoading = false
         }
         
         self.isLoading = true
+        self.model = nil
+        
         do {
-            let result = try await CharactersService.shared.fetch(name: characterName)
-            self.model = result
+            var character = try await fetchCharacter()
+            var onlinePlayers: [OnlinePlayersModel] = []
+            
+            if let charWorld = character.character.world {
+                onlinePlayers = await playersOnlineFrom(world: charWorld)
+            }
+            
+            character.isOnline = onlinePlayers.contains(where: { $0.name == characterName })
+            self.model = character
         } catch {
-            print("Some error occured on characters search view model: \(error)")
+            print("Some error occured on characters search view model, fetchAllInfo: \(error)")
             self.hasError = true
         }
     }
     
-    func playersOnlineFrom(world name: String) async -> [OnlinePlayersModel] {
-        defer {
-            self.isLoading = false
+    private func fetchCharacter() async throws -> CharacterModel {
+        do {
+            let result = try await CharactersService.shared.fetch(name: characterName)
+            return result
+        } catch {
+            print("Some error occured on characters search view model: \(error)")
+            throw error
         }
-        
-        self.isLoading = true
+    }
+    
+    private func playersOnlineFrom(world name: String) async -> [OnlinePlayersModel] {
         do {
             let model = try await WorldsService.shared.fetch(world: name)
             return model.onlinePlayers
